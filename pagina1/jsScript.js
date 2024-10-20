409b463d35a938bc341a2ab5a69a6be80a1b2adc
cristhi2404 <72783261+cristhi2404@users.noreply.github.com>
2024-10-20 17:48:08 -0500

    Delete pagina1 directory

--- pagina1/jsScript.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.button').addEventListener('click', function() {
-    const Pestaña = document.querySelector('.Pestaña');
-    const Botonimg = document.querySelector('.Botonimg')
-    Pestaña.classList.toggle('opened');
-
-    Botonimg.classList.toggle('rotate')
-});
